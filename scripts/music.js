5c6769228324042351ff421501b6cc27a0127b66
mathewmanton <mathewmanton@hotmail.com>
2015-04-17 16:56:34 -0400

    music

---
+++ scripts/music.js
@@ -0,0 +1,34 @@
+﻿
+
+    var audio = document.getElementById("gamemusic")
+    
+    audio.play();
+    audio.loop = true;
+    
+    function ChangeAudio()
+    {
+        audio.pause();        
+        audio.src = "Assets/Music/Level1music.mp3";
+        audio.volume = 0.5;
+        audio.play();
+        
+    }
+    
+    function MuteAudio()
+    {
+        var ismuted = false;
+        if(ismuted)
+        {
+            audio.muted = false;
+            ismuted = false;
+        }
+        else if(!ismuted)
+        {
+            audio.muted = true;
+            ismuted = true;
+            
+        }
+    }
+
+  
+
